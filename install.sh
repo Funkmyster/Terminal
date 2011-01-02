25b495c8d829a5ae401416d32596a516c8f75526
Jacob Silva <questforall@gmail.com>
2011-01-02 15:57:48 -0600

    Install Script - Init

---
+++ install.sh
@@ -0,0 +1,6 @@
+cp .bashrc	/home/jsilva/.bashrc
+cp .pythonrc.py	/home/jsilva/.pythonrc.py
+cp .screenrc	/home/jsilva/.screenrc
+cp .screenworkrc	/home/jsilva/.screenworkrc
+cp .screenworkstationrc	/home/jsilva/.screenworkstationrc
+cp .vimrc	/home/jsilva/.vimrc
