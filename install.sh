0c2cd6dc75b455d815e8da3d167081e1fa3cc65c
Jacob Silva <questforall@gmail.com>
2011-01-13 11:04:22 -0600

    Git - Confs

--- install.sh
+++ install.sh
@@ -6,3 +6,7 @@ cp .screenrc		~/.screenrc
 cp .screenworkrc	~/.screenworkrc
 cp .screenworkstationrc	~/.screenworkstationrc
 cp .vimrc		~/.vimrc
+
+
+cp .gitconfig		~/.gitconfig
+cp .git.commit.template ~/.git.commit.template
