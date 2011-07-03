ee75ec1e87422e7236f57d45e7dce9b312820c53
Jacob Silva <questforall@gmail.com>
2011-07-03 01:50:07 -0500

    Install Script

--- install.sh
+++ install.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
-cp .bashrc		~/.bashrc
-cp .pythonrc.py		~/.pythonrc.py
-cp .screenrc		~/.screenrc
-cp .screenfarmrc	~/.screenfarmrc
-cp .screenworkrc	~/.screenworkrc
-cp .screenworkstationrc	~/.screenworkstationrc
-cp .vimrc		~/.vimrc
-cp .inputrc		~/.inputrc
+rm ~/.inputrc
+ln -s ~/Libs/Terminal/readline/inputrc ~/.inputrc
 
+rm ~/.bashrc
+ln -s ~/Libs/Terminal/bash/bashrc  ~/.bashrc
 
-cp .gitconfig		~/.gitconfig
-cp .git.commit.template ~/.git.commit.template
+rm ~/.pythonrc.py
+ln -s ~/Libs/Terminal/python/pythonrc.py ~/.pythonrc.py
+
+rm ~/.screenrc
+ln -s ~/Libs/Terminal/screen/screenrc ~/.screenrc
+
+rm ~/.screenfarmrc
+ln -s ~/Libs/Terminal/screen/screenfarmrc ~/.screenfarmrc
+
+rm ~/.gitconfig
+ln -s ~/Libs/Terminal/git/gitconfig ~/.gitconfig
+
+rm ~/.git.commit.template
+ln -s ~/Libs/Terminal/git/git.commit.template ~/.git.commit.template
+
+rm ~/.vimrc
+ln -s ~/Libs/Terminal/vim/vimrc ~/.vimrc
+
+rm -rf ~/.vim
+ln -s ~/Libs/Terminal/vim/vim ~/.vim
